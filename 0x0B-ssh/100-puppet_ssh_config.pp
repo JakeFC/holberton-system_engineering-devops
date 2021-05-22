e4b06b98d8beb6e755c78614f44d48b329ea76a1
JakeFC <jacobchavera@yahoo.com>
2021-05-22 11:39:36 +0000

    commented file_line method since it doesn't work, added newline character to separate content on one line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,17 @@
 # Changes client SSH config file to be able to connect w/o a password
-file { '/etc/ssh/ssh_config':
-       ensure => 'present',
-       content => 'PasswordAuthentication no,
-       	       	  IdentityFile -/.ssh/holberton'
+
+file { "/etc/ssh/ssh_config":
+    ensure  => 'present',
+    content => 'PasswordAuthentication no\nIdentityFile -/.ssh/holberton',
 }
+
+#file_line { 'Remove pass auth':
+#    ensure => 'present',
+#    path   => '/etc/ssh/ssh_config',
+#    line   => 'PasswordAuthentication no',
+#}
+#file_line { 'Set key file':
+#    ensure => 'present',
+#    path   => '/etc/ssh/ssh_config',
+#    line   => 'IdentityFile -/.ssh/holberton',
+#}
