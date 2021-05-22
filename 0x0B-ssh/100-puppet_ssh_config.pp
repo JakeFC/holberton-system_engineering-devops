2d4c54a84cf799a084a87cace4aed33991000ab4
JakeFC <jacobchavera@yahoo.com>
2021-05-22 11:06:17 +0000

    switched from augeas changes to file content

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,8 @@
 # Changes client SSH config file to be able to connect w/o a password
-service { 'ssh':
-	ensure => running
-}
-
-augeas { 'ssh config':
-       context => '/etc/ssh/ssh_config',
-       changes => [
-       	       'set PasswordAuthentication no',
-	       'set HostName 54.221.48.50',
-	       'set User ubuntu',
-	       'set IdentityFile -/.ssh/holberton'
+file { '/etc/ssh/ssh_config':
+       ensure => 'present',
+       content => [
+       	       'PasswordAuthentication no',
+	       'IdentityFile -/.ssh/holberton'
 	       ]
 }
