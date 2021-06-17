86490d3a2985b91698c95b9b7d2429a13209b30d
JakeFC <jacobchavera@yahoo.com>
2021-06-17 02:52:18 +0000

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module for save_all_to_json function """
+
+import json
+import requests
+
+
+def save_all_to_json():
+    """ Using a REST API, returns info about all employees' TODOS
+    """
+
+    users_and_tasks = {}
+
+    users_json = requests.get('https://jsonplaceholder.typicode.com/users')\
+        .json()
+    todos_json = requests.get('https://jsonplaceholder.typicode.com/todos')\
+        .json()
+
+    user_info = {}
+    for user in users_json:
+        user_info[user['id']] = user['username']
+
+    for task in todos_json:
+        if users_and_tasks.get(task['userId'], False) is False:
+            users_and_tasks[task['userId']] = []
+        task_dict = {}
+        task_dict['username'] = user_info[task['userId']]
+        task_dict['task'] = task['title']
+        task_dict['completed'] = task['completed']
+        users_and_tasks[task['userId']].append(task_dict)
+
+    with open('todo_all_employees.json', 'w') as jsonFile:
+        json.dump(users_and_tasks, jsonFile)
+
+    return 0
+
+if __name__ == "__main__":
+    save_all_to_json()
